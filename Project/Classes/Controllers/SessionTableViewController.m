e06c09405f004561a3396011c2c2462c93b87e34
Katsuyoshi Ito <kito@itosoft.com>
2009-07-08 21:27:12 +0900

    Autorotateに対応

--- Project/Classes/Controllers/SessionTableViewController.m
+++ Project/Classes/Controllers/SessionTableViewController.m
@@ -84,13 +84,10 @@
 }
 */
 
-/*
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
-*/
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
