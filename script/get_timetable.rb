5a1e5c77bc047c99af78fd7b5da24650e63abc26
Katsuyoshi Ito <kito@itosoft.com>
2010-08-08 20:45:43 +0900

    シンボルから文字に

--- script/get_timetable.rb
+++ script/get_timetable.rb
@@ -7,6 +7,7 @@ require File.expand_path(File.dirname(__FILE__) + '/simple-json')
 #@store_dir = @base_dir + "/../../../public/irubykaigi/2010/"
 @store_dir = @base_dir + "/"
 @filename = @store_dir + "timetables.json"
+@tmpfile = @store_dir + "timetables.tmp"
 
 class String
   def strip_ns
@@ -17,15 +18,15 @@ end
 
 def get_lightningtalks content
   talks = content.search("li").collect do |l|
-    talk = { :speakers => []}
+    talk = { "speakers" => []}
     t = l.text.strip_ns
     t.gsub!('Urabe, Shyouhei', 'Urabe Shyouhei')
     t.split(",").each_with_index do |e, i|
       case i
       when 0
-        talk[:title] = e.strip_ns
+        talk["title"] = e.strip_ns
       when 1
-        talk[:speakers] << { :name =>  e.split('(')[0].strip_ns }
+        talk["speakers"] << { :name =>  e.split('(')[0].strip_ns }
       end
     end
     talk
@@ -39,15 +40,15 @@ def get_foot_section content, day
   return nil if th == ""
   
   a = th.strip_ns.split('-')
-  session = { :start_at => a[0].strip_ns, :end_at => a[1].strip_ns }
+  session = { "start_at" => a[0].strip_ns, "end_at" => a[1].strip_ns }
   
   a = content.search('td').text.strip_ns.split(',')
-  session[:title] = a[0].strip_ns
+  session["title"] = a[0].strip_ns
   /([^\(]+)(\([^\)]+\))?/ =~ a[1].strip_ns.gsub("於 ", "").gsub(/\s*at\s*/, "")
-  session[:room] = $1;
-  session[:summary] = $2 if $2
-  session[:type] = 'party'
-  session[:code] = "#{day}|#{$1}|#{session[:start_at]}"
+  session["room"] = $1;
+  session["summary"] = $2 if $2
+  session["type"] = 'party'
+  session["code"] = "#{day}|#{$1}|#{session["start_at"]}"
   session
 end
 
@@ -59,35 +60,35 @@ def get_session href
 
   content = page.search("div#content")[0]
   
-  session = { :code => url.split('/').last }
+  session = { "code" => url.split('/').last }
   
   key = nil
   index = 0
   content.children.each do |c|
     case c.name
     when "h1"
-      session[:title] = c.text.strip_ns
-      case session[:title]
+      session["title"] = c.text.strip_ns
+      case session["title"]
       when "Lightning Talks"
-        session[:type] = 'lightning_talks'
+        session["type"] = 'lightning_talks'
       when "Opening"
-        session[:type] = 'opening'
+        session["type"] = 'opening'
       when "Closing"
-        session[:type] = 'closing'
+        session["type"] = 'closing'
       end
 #p session
       key = nil
     when "h2"
       case c.text.strip_ns
       when "スピーカー", "Speaker(s)"
-        key = :speakers
+        key = "speakers"
       when "概要", "Abstract"
-        key = :summary
+        key = "summary"
         index = 0
       when "日程", "Date"
         key = :time
       when "会場", "Room"
-        key = :room
+        key = "room"
       when "話す自然言語", "Spoken Language"
         key = nil
       else
@@ -96,24 +97,24 @@ def get_session href
     when "p"
       if session[key] == nil
         case key
-        when :speakers
+        when "speakers"
           n = c.text.split('/').collect { |s| { :name => s.strip_ns.split('(').first } }
-          session[:speakers] = n
-        when :summary
-          session[:summary] = c.text.strip_ns if index == 1
+          session["speakers"] = n
+        when "summary"
+          session["summary"] = c.text.strip_ns if index == 1
           index += 1
         when :time
           t = c.text.strip_ns.split(' ')
-          session[:start_at] = t[1]
-          session[:end_at] = t[3]
-        when :room
-          session[:room] = c.text.strip_ns
+          session["start_at"] = t[1]
+          session["end_at"] = t[3]
+        when "room"
+          session["room"] = c.text.strip_ns
         end
       end
     end
   end
 
-  session[:lightning_talks] = get_lightningtalks content if session[:type] == "lightning_talks"
+  session["lightning_talks"] = get_lightningtalks content if session["type"] == "lightning_talks"
   
   session
 end
@@ -129,7 +130,7 @@ def get_sessions timetable, day
   times.collect! do |l|
     a = l.split("|");
     if a.size == 2
-      { :start_at => a[0].strip_ns, :end_at => a[1].strip_ns }
+      { "start_at" => a[0].strip_ns, "end_at" => a[1].strip_ns }
     else
       nil
     end
@@ -153,30 +154,30 @@ def get_sessions timetable, day
       end
 
       # マーク
-      rowspan = td[:rowspan] ? td[:rowspan].to_i : 1
+      rowspan = td['rowspan'] ? td['rowspan'].to_i : 1
       rowspan.times {|i| rowspan_flags[row + i][col] = 1 }
 
       session = nil
 
-      case td[:class]
+      case td['class']
       when "room", "room_hall"
         a = td.search('a')
-        session = get_session(a.first[:href]) if a && a.first
-        session[:speakers] = td.search('p.speaker').text.split("/").collect{|e| { :name => e.strip_ns } } if session
+        session = get_session(a.first['href']) if a && a.first
+        session["speakers"] = td.search('p.speaker').text.split("/").collect{|e| { :name => e.strip_ns } } if session
 #p session
       when "break"
         s = td.search('div.session')
         session = {
-            :title => s.search('p.title').text.strip_ns,
-            :start_at => times[row][:start_at],
-            :end_at => times[row + rowspan - 1][:end_at],
-            :room => rooms[col],
-            :type => 'break',
-            :code => "#{day}|#{rooms[col]}|#{times[row - 1][:start_at]}"
+            "title" => s.search('p.title').text.strip_ns,
+            "start_at" => times[row]["start_at"],
+            "end_at" => times[row + rowspan - 1]["end_at"],
+            "room" => rooms[col],
+            "type" => 'break',
+            "code" => "#{day}|#{rooms[col]}|#{times[row - 1]["start_at"]}"
         }
       when "empty", nil
       else
-        $stderr.puts "unknown class '#{td[:class]}'"
+        $stderr.puts "unknown class '#{td['class']}'"
       end
         
       sessions << session if session
@@ -189,7 +190,7 @@ def get_sessions timetable, day
     session = get_foot_section foot, day
 #p session
     if session
-      rooms << session[:room] unless rooms.include? session[:room]
+      rooms << session["room"] unless rooms.include? session["room"]
 #puts rooms
       sessions << session
     end
@@ -216,19 +217,26 @@ def get_timetables url
   end
   
   { :timetables => timetables,
-    :rooms => rooms }
+    "rooms" => rooms }
 end
 
 def get_all_timetables
-   { :ja => get_timetables('http://rubykaigi.org/2010/ja/timetable'),
-     :en => get_timetables('http://rubykaigi.org/2010/en/timetable'),
+   { 'ja' => get_timetables('http://rubykaigi.org/2010/ja/timetable'),
+     'en' => get_timetables('http://rubykaigi.org/2010/en/timetable'),
      :updated_at => Time.now.strftime("%Y-%m-%d %X %z") }
 end
 
 
 timetables = get_all_timetables
+
+# 一回書き出してパースしないと順番が同じにならない？
+File.open(@tmpfile, 'w') do |f|
+  f.write WebAPI::JsonBuilder.new.build(timetables)
+end
+new_timetables = WebAPI::JsonParser.new.parse(File.read(@tmpfile))
+
 old_timetables = File.exists?(@filename) ? WebAPI::JsonParser.new.parse(File.read(@filename)) : {}
-unless timetables[:ja] == old_timetables["ja"] && timetables[:en] == old_timetables["en"]
+unless new_timetables['ja'] == old_timetables['ja'] && new_timetables['en'] == old_timetables['en']
   File.open(@filename, 'w') do |f|
     f.write(WebAPI::JsonBuilder.new.build(timetables))
   end
